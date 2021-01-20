ba973dd204d571907ac1ade87726249877b1a84f
Cirno_null <1173911191@qq.com>
2021-01-20 21:01:17 +0800

    Update 测试.lua

--- Lua/测试.lua
+++ Lua/测试.lua
@@ -10,25 +10,15 @@ function read_file(path)
     return text
 end
 --[[↑读取对应路径的文件]]
-function write_file(path, text)
-    local file = io.open(dice.UTF8toGBK(path), "w") -- 以只写的方式
-    file.write(file, text) -- 写入内容
-    io.close(file) -- 关闭文件
-end
---[[↑写入对应的文件]]
 function test(Msg)
-    local file_path = dice.DiceDir() .. "\\publicdeck\\无尽的挂机.json"
+    local file_path = dice.DiceDir() .. "\\conf\\CustomReply.json"
     local text_json = read_file(file_path)
     local tbl, pos, err = json.decode(text_json, 1, nil)
     local fixed_table = {}
     for key, value in pairs(tbl) do
         table.insert(fixed_table, key)
-        tbl[key] = tbl[nil] -- 回收内存
     end
     tbl = nil -- 回收内存
-    file_path = dice.DiceDir() .. "\\conf\\ttttttttttttttttttt.json"
-    local text = json.encode(fixed_table, {indent = true}) --转成json
-    write_file(file_path, text)
-    return "完毕"
+    return table.concat( fixed_table,",")
 end
-command["测试"] = "test"
+command["当前tia的回复词"] = "test"
