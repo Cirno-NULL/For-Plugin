7014835aa12682363f87332d8acbb23ce38cfbe4
Cirno_null <1173911191@qq.com>
2021-01-27 17:47:14 +0800

    Update 测试.lua

--- Lua/测试.lua
+++ Lua/测试.lua
@@ -1,26 +1,26 @@
 command = {}
 local json = require("dkjson")
-function read_file(path)
-    local text = ""
-    local file = io.open(dice.UTF8toGBK(path), "r") -- 打开了文件读写路径
-    if (file ~= nil) then -- 如果文件不是空的
-        text = file.read(file, "*a") -- 读取内容
-        io.close(file) -- 关闭文件
-    end
-    return text
-end
+-- function Read_File(path)
+    -- local text = ""
+    -- local file = io.open(dice.UTF8toGBK(path), "r") -- 打开了文件读写路径
+    -- if (file ~= nil) then -- 如果文件不是空的
+        -- text = file.read(file, "*a") -- 读取内容
+        -- io.close(file) -- 关闭文件
+    -- end
+    -- return text
+-- end
 --[[↑读取对应的文件]]
-function write_file(path, text)
-    local file = io.open(dice.UTF8toGBK(path), "w") -- 以只写的方式
-    file.write(file, text) -- 写入内容
-    io.close(file) -- 关闭文件
-end
+-- function Write_File(path, text)
+    -- local file = io.open(dice.UTF8toGBK(path), "w") -- 以只写的方式
+    -- file.write(file, text) -- 写入内容
+    -- io.close(file) -- 关闭文件
+-- end
 --[[↑写入对应的文件]]
-function delete_file(path)
-    os.remove(dice.UTF8toGBK(path))
-end
+-- function Delite_File(path)
+    -- os.remove(dice.UTF8toGBK(path))
+-- end
 --[[↑删除文件,请谨慎使用]]
-function create_deck()
-    
-end
-command["创建牌堆"] = "create_deck"
+-- function Create_Deck(Msg)
+    -- 
+-- end
+command["当前tia的回复词"] = "Create_Deck"
