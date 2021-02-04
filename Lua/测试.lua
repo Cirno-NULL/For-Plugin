47a4aa82897d15e3643415ef848d09418bbbff0e
Cirno_null <1173911191@qq.com>
2021-02-04 08:04:58 +0800

    Update 测试.lua

--- Lua/测试.lua
+++ Lua/测试.lua
@@ -1,11 +1,7 @@
 command = {}
--- local json = require("dkjson")
-sqlite3 = require('lsqlite3')
-function CreateDeck(Msg)
-    local file_path = dice.DiceDir() .. "\\conf\\CustomReply.json"
-    local text_json = null.ReadFile(file_path)
-    return #text_json
+function CreateDeck()
+    require("sqlite3")
+    return "完了"
 end
--- test_module.lua 文件
--- module 模块为上文提到到 module.lua
-command["新建牌堆(.+)"] = "CreateDeck"
+
+command["测试"] = "CreateDeck"
