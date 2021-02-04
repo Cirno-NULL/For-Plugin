162fadb3e54c898511bebe1fe3990d4d32a998f2
Cirno_null <1173911191@qq.com>
2021-02-04 08:21:12 +0800

    Update 测试.lua

--- Lua/测试.lua
+++ Lua/测试.lua
@@ -1,7 +1,7 @@
-command = {}
-function CreateDeck()
-    require("sqlite3")
+-- command = {}
+-- function CreateDeck()
+    sqlite3 = require "lib.sqlite3"
     return "完了"
-end
+-- end
 
-command["测试"] = "CreateDeck"
+-- command["测试"] = "CreateDeck"
