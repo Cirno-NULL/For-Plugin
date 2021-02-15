4199f9a49a347f9f119cada11f5df92e28d1e4bd
Cirno_null <1173911191@qq.com>
2021-02-15 12:14:05 +0800

    Revert "Revert "咕咕语录""
    This reverts commit 8fc27038c3b9a4c5ddcc889b769abe8a4c98294b.

--- lua/lua/点歌.lua
+++ lua/lua/点歌.lua
@@ -17,26 +17,28 @@ function write_file(path, text)
 end
 --[[↑写入对应的文件]]
 function test(Msg)
-    local base_path = dice.DiceDir() .. "\\user\\Cirno_plugin\\163music\\" --搜歌路径
-    local dir_path = base_path .. "discuss\\" --热评路径
-    local file_path = base_path .. "163music" .. ".json" --缓存歌曲路径
+    local base_path = dice.DiceDir() .. "\\user\\Cirno_plugin\\public\\163music\\" --搜歌路径
+    local dir_path = base_path .. "why\\" --热评路径
     dice.mkDir(dir_path)
-    local songname = dice.UrlEncode(Msg.str[1])
+    local file_path = base_path .. "163music" .. ".json" --缓存歌曲路径
+    local rv = ""
+
+    local songname = dice.UrlEncode(Msg.str[1]) -- 转成url格式
     local url = "https://musicapi.leanapp.cn/search?keywords=" .. songname .. "&limit=1" --歌曲api
     dice.fDownWebPage(url, file_path) --下载
     local songinfo = read_file(file_path) -- 读取
     local songinfo = json.decode(songinfo, 1, nil) -- 解成table
-    local rv = ""
-    if songinfo.result.songs == nil then
+    if songinfo.result.songs == nil then -- 如果没有结果
         return "{self}找不到{nick}想要听的歌QvQ"
     end
     local songid = songinfo.result.songs[1].id --读取songid
     rv = rv .. "https://y.music.163.com/m/song/" .. songid --合成网址
 
-    local songinfo = read_file(file_path) --优先读取缓存文件
+    file_path = dir_path .. songid .. ".json" -- 合成热评路径
+    songinfo = read_file(file_path) --优先读取缓存文件
+
     if songinfo == "" or songinfo == nil then
         url = "http://music.163.com/api/v1/resource/comments/R_SO_4_" .. songid .. "?limit=10&offset=0" --热评api
-        file_path = dir_path .. songid .. ".json" -- 合成热评路径
         dice.fDownWebPage(url, file_path) -- 下载热评
         songinfo = read_file(file_path) -- 读取
     end
